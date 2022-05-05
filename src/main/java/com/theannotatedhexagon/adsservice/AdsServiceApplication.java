c9d43e02391b4ea4e7ef98a57ed9b4cf4a731533
jovankricka <jovankricka@gmail.com>
2022-05-05 16:54:52 +0200

    Initial commit

---
+++ src/main/java/com/theannotatedhexagon/adsservice/AdsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.theannotatedhexagon.adsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdsServiceApplication.class, args);
+	}
+
+}
